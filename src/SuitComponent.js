3aae3cb722274f1b7f2f9c89d1721c6f06b16ad4
Andrew Brower <63933741+abrower10612@users.noreply.github.com>
2022-02-09 08:11:14 -0700

    slight adjustment to indentation

--- src/SuitComponent.js
+++ src/SuitComponent.js
@@ -3,14 +3,14 @@ import React from 'react'
 const SuitComponent = ({ suit, cards, classes, sortOrder }) => {
   return (
     <>
-        {
-          cards
-            .filter(card => card.suit === suit)
-            .sort((a, b) => { return sortOrder.indexOf(a.value) - sortOrder.indexOf(b.value) })
-            .map((suit, index) => {
-                return <img key={index} src={suit.image} alt="" className={ suit.value === "QUEEN" ? classes.queen : classes.card }/>
-            })
-        }
+      {
+        cards
+          .filter(card => card.suit === suit)
+          .sort((a, b) => { return sortOrder.indexOf(a.value) - sortOrder.indexOf(b.value) })
+          .map((suit, index) => {
+              return <img key={index} src={suit.image} alt="" className={ suit.value === "QUEEN" ? classes.queen : classes.card }/>
+          })
+      }
     </>
   )
 }
